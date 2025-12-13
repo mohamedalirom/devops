63ab82792103b42d2e1f24eb8502f37e720520f9
mohamedalirom <mohamedali.romdhane@esprit.tn>
2025-12-13 18:21:42 +0100

    Fix tests: force H2 datasource with AutoConfigureTestDatabase

--- src/test/java/tn/esprit/studentmanagement/AbstractTest.java
+++ src/test/java/tn/esprit/studentmanagement/AbstractTest.java
@@ -1,9 +1,11 @@
 package tn.esprit.studentmanagement;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public abstract class AbstractTest {
 }
